dc14fd01fefd2f9c998cf862b145d31772ec2ba2
rkdwn <kj2693119@gmail.com>
2022-12-21 10:41:22 +0900

    [RESET] reset project

--- scripts/link-modules.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import webpackPaths from "../.webpack/webpack.paths";
-
-const srcNodeModulesPath = webpackPaths.srcNodeModulesPath;
-const appNodeModulesPath = webpackPaths.appNodeModulesPath;
-
-if (!fs.existsSync(srcNodeModulesPath) && fs.existsSync(appNodeModulesPath)) {
-  fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, "junction");
-}
